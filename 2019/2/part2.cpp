e270b33c5bb0e0d084af3ba2202c7ee9f4c2647f
Anders Wind <awia00@gmail.com>
2019-12-02 22:39:31 +0100

    Solved 2019-02 Part 2

--- 2019/2/part2.cpp
+++ 2019/2/part2.cpp
@@ -27,48 +27,54 @@ void print(std::vector<int> arr)
     }
 }
 
-// 164 too low
+int execute_codes(std::vector<int> codes, int noun, int verb) {
+    codes[1] = noun;
+    codes[2] = verb;
+    auto i = 0;
+    while(i < codes.size())
+    {
+        const auto code = codes[i];
+        if (code == 99)
+        {
+            break;
+        }
+        else if (code == 1)
+        {
+            auto first = codes.at(i + 1);
+            auto second = codes.at(i + 2);
+            auto to = codes.at(i + 3);
+            codes.at(to) = codes.at(first) + codes.at(second);
+            i += 4;
+        }
+        else if (code == 2)
+        {
+            auto first = codes.at(i + 1);
+            auto second = codes.at(i + 2);
+            auto to = codes.at(i + 3);
+            codes.at(to) = codes.at(first) * codes.at(second);
+            i += 4;
+        }
+        else {
+            i = -1;
+            break;
+        }
+    }
+    return codes[0];
+}
+// 490668 too high
 int main()
 {
     auto codes = read_input();
     for (auto noun = 0; noun < 100; noun++)
     {
-        codes[1] = noun;
         for (auto verb = 0; verb < 100; verb++)
         {
-            codes[2] = verb;
-            for (auto i = 0; i < codes.size();)
-            {
-                const auto code = codes[i];
-                if (code == 99)
-                {
-                    break;
-                }
-                else if (code == 1)
-                {
-                    auto first = codes.at(i + 1);
-                    auto second = codes.at(i + 2);
-                    auto to = codes.at(i + 3);
-                    codes.at(to) = codes.at(first) + codes.at(second);
-                    i += 4;
-                }
-                else if (code == 2)
-                {
-                    auto first = codes.at(i + 1);
-                    auto second = codes.at(i + 2);
-                    auto to = codes.at(i + 3);
-                    codes.at(to) = codes.at(first) * codes.at(second);
-                    i += 4;
-                }
-            }
-            if (codes[0] == 19690720)
+            auto result = execute_codes(codes, noun, verb);
+            if (result == 19690720)
             {
                 std::cout << 100 * noun + verb << std::endl;
                 return 0;
             }
         }
-        std::cout << "next" << std::endl;
     }
-
-    std::cout << codes[0] << std::endl;
 }
