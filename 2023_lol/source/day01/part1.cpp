fe18c8eef144ecfe302cddd5039829d2010bab16
Anders Wind <awia00@gmail.com>
2023-12-04 22:54:21 +0100

    fixing git

---
+++ 2023_lol/source/day01/part1.cpp
@@ -0,0 +1,14 @@
+#include <doctest/doctest.h>
+
+namespace wind
+{
+
+TEST_SUITE("part1")
+{
+    TEST_CASE("sample")
+    {
+        CHECK(true);
+    }
+}
+
+}  // namespace wind
---
+++ 2023_lol/source/day01/part2.cpp
@@ -0,0 +1,14 @@
+#include <doctest/doctest.h>
+
+namespace wind
+{
+
+TEST_SUITE("part2")
+{
+    TEST_CASE("sample")
+    {
+        CHECK(true);
+    }
+}
+
+}  // namespace wind
